300b9635020b67b19bf4ab20394a74a55afdaf37
soliury <soliury@pm.me>
2020-10-25 01:21:07 +0800

    bugfix: when use custom price api, get ref_price meet some problem

--- hummingbot/strategy/pure_market_making/pure_market_making.pyx
+++ hummingbot/strategy/pure_market_making/pure_market_making.pyx
@@ -36,6 +36,7 @@ from .asset_price_delegate import AssetPriceDelegate
 from .inventory_skew_calculator cimport c_calculate_bid_ask_ratios_from_base_asset_ratio
 from .inventory_skew_calculator import calculate_total_order_size
 from .order_book_asset_price_delegate cimport OrderBookAssetPriceDelegate
+from .api_asset_price_delegate cimport APIAssetPriceDelegate
 
 
 NaN = float("nan")
@@ -538,6 +539,8 @@ cdef class PureMarketMakingStrategy(StrategyBase):
             ref_price = float("nan")
             if market == self._market_info.market and self._asset_price_delegate is None:
                 ref_price = self.get_price()
+            elif type(self._asset_price_delegate) is APIAssetPriceDelegate:
+                ref_price = self._asset_price_delegate.get_mid_price()
             elif market == self._asset_price_delegate.market and self._price_type is not PriceType.LastOwnTrade:
                 ref_price = self._asset_price_delegate.get_price_by_type(self._price_type)
             markets_data.append([
